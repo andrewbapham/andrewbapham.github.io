deb0197ddcb3b471b36b8d632103974d57ad863e
Andrew Pham <andrewbapham@outlook.com>
2022-09-05 14:44:43 -0400

    Added socials sidebar
    Added socials sidebar and moved colors to variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Intro from "./assets/components/Intro";
 import "./App.css";
+import TransitionButton from "./assets/components/TransitionButton";
+import SocialSidebar from "./assets/components/SocialSidebar";
 
 function App() {
 	//const [count, setCount] = useState(0);
@@ -8,6 +10,79 @@ function App() {
 	return (
 		<div className="App">
 			<Intro />
+			<TransitionButton />
+			<SocialSidebar />
+			<div className="test">
+				<br></br>
+				<br></br>
+				&nbsp;
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				&nbsp;
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+				&nbsp;
+			</div>
 		</div>
 	);
 }
