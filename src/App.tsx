b2f3c3b9a1ae9585431e3e16060d457e38637174
Andrew Pham <andrewbapham@outlook.com>
2023-10-08 15:13:38 -0400

    Change social link and resume

--- src/App.tsx
+++ src/App.tsx
@@ -10,56 +10,56 @@ import Projects from "./assets/components/Projects";
 import NavBar from "./assets/components/NavBar";
 
 function App() {
-	const blur = {
-		filter: "blur(5px) brightness(0.7);",
-	};
-	const [navExpanded, setNavExpanded] = useState(false);
-	function handleNavClick(): void {
-		let val: boolean = !navExpanded;
-		setNavExpanded(val);
-	}
+  const blur = {
+    filter: "blur(5px) brightness(0.7);",
+  };
+  const [navExpanded, setNavExpanded] = useState(false);
+  function handleNavClick(): void {
+    let val: boolean = !navExpanded;
+    setNavExpanded(val);
+  }
 
-	useEffect(() => {
-		const handleResize = () => {
-			if (window.innerWidth > 767) {
-				console.log("close menu");
-				setNavExpanded(false);
-			}
-		};
-		window.addEventListener("resize", handleResize);
-		return () => window.removeEventListener("resize", handleResize);
-	}, []);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 767) {
+        console.log("close menu");
+        setNavExpanded(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-	return (
-		<div className="App">
-			<NavBar handleNavClick={handleNavClick} navExpanded={navExpanded} />
-			<main className={navExpanded ? "blur" : ""}>
-				<div id="content">
-					<section id="intro-page">
-						<Intro />
-						<TransitionButton target={"#about"} />
-					</section>
-					<section id="about">
-						<About />
-						<TransitionButton target={"#experience"} />
-					</section>
-					<section id="experience">
-						<Experience />
-						<TransitionButton target={"#projects"} />
-					</section>
-					<section id="projects">
-						<Projects />
-						<TransitionButton
-							className="bottom-transition"
-							target={"#intro-page"}
-						/>
-					</section>
+  return (
+    <div className="App">
+      <NavBar handleNavClick={handleNavClick} navExpanded={navExpanded} />
+      <main className={navExpanded ? "blur" : ""}>
+        <div id="content">
+          <section id="intro-page">
+            <Intro />
+            <TransitionButton target={"#about"} />
+          </section>
+          <section id="about">
+            <About />
+            <TransitionButton target={"#experience"} />
+          </section>
+          <section id="experience">
+            <Experience />
+            <TransitionButton target={"#projects"} />
+          </section>
+          <section id="projects">
+            <Projects />
+            <TransitionButton
+              className="bottom-transition"
+              target={"#intro-page"}
+            />
+          </section>
 
-					<SocialSidebar />
-				</div>
-			</main>
-		</div>
-	);
+          <SocialSidebar />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/assets/components/SocialSidebar.tsx
+++ src/assets/components/SocialSidebar.tsx
@@ -3,69 +3,69 @@ import { motion } from "framer-motion";
 import "./component-styles/SocialSidebar.css";
 
 function SocialSidebar() {
-	return (
-		<div className="side-nav">
-			<ul>
-				<li>
-					<a
-						href="https://github.com/andrewbapham"
-						aria-label="GitHub"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<motion.svg
-							xmlns="http://www.w3.org/2000/svg"
-							role="img"
-							className="github-icon"
-							whileHover={{ scale: 1.2 }}
-						>
-							<title>GitHub</title>
-							<path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-						</motion.svg>
-					</a>
-				</li>
-				<li>
-					<a
-						href="https://www.linkedin.com/in/andrew-pham-856553221/"
-						aria-label="Linkedin"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<motion.svg
-							xmlns="http://www.w3.org/2000/svg"
-							role="img"
-							viewBox="0 0 24 24"
-							className="feather feather-linkedin"
-							whileHover={{ scale: 1.2 }}
-						>
-							<title>LinkedIn</title>
-							<path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-							<rect x="2" y="9" width="4" height="12"></rect>
-							<circle cx="4" cy="4" r="2"></circle>
-						</motion.svg>
-					</a>
-				</li>
-				<li>
-					<a
-						href="mailto:andrewbapham@outlook.com"
-						aria-label="Email"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<motion.svg
-							xmlns="http://www.w3.org/2000/svg"
-							role="img"
-							viewBox="0 0 24 24"
-							whileHover={{ scale: 1.2 }}
-						>
-							<path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-							<polyline points="22,6 12,13 2,6" />
-						</motion.svg>
-					</a>
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div className="side-nav">
+      <ul>
+        <li>
+          <a
+            href="https://github.com/andrewbapham"
+            aria-label="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              className="github-icon"
+              whileHover={{ scale: 1.2 }}
+            >
+              <title>GitHub</title>
+              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+            </motion.svg>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/andrewbapham/"
+            aria-label="Linkedin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              className="feather feather-linkedin"
+              whileHover={{ scale: 1.2 }}
+            >
+              <title>LinkedIn</title>
+              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+              <rect x="2" y="9" width="4" height="12"></rect>
+              <circle cx="4" cy="4" r="2"></circle>
+            </motion.svg>
+          </a>
+        </li>
+        <li>
+          <a
+            href="mailto:andrewbapham@outlook.com"
+            aria-label="Email"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.svg
+              xmlns="http://www.w3.org/2000/svg"
+              role="img"
+              viewBox="0 0 24 24"
+              whileHover={{ scale: 1.2 }}
+            >
+              <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+              <polyline points="22,6 12,13 2,6" />
+            </motion.svg>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SocialSidebar;
