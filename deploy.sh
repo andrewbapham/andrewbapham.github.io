e7b153a798f51e25eac97a25ab519f93a8149dba
Andrew Pham <andrewbapham@outlook.com>
2024-03-16 18:52:46 -0400

    Add deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
+echo "Checking out master..."
+
 git checkout master
-npm run build
+
+echo "Building site..."
+npm run --silent build
+
+echo "Pushing to branch gh-pages..."
 git add dist -f
 git commit -m "Deploy"
 git subtree push --prefix dist origin gh-pages
+
+echo "Done!"
