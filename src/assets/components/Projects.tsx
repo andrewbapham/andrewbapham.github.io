f234ffc6131d9e9bd88fdf2a18e7225405d84698
Andrew Pham <andrewbapham@outlook.com>
2024-03-16 18:47:39 -0400

    Add Project components and section

--- src/assets/components/Projects.tsx
+++ src/assets/components/Projects.tsx
@@ -1,39 +1,40 @@
 import React from "react";
+import Slideshow from "./Slideshow";
+import ProjectItem from "./ProjectItem";
 
 function Projects() {
   return (
     <div className="main-content section-container">
       <h1 className="section-header">Projects</h1>
-      <p>Currently a work in progress, come back later to check it out!</p>
       <div className="project-container">
-        <div className="project-slide">
-          <h2 className="project-title">
-            <a
-              href="https://github.com/andrewbapham/ehotels-app"
-              target={"_blank"}
-            >
-              E-hotels App
-            </a>
-          </h2>
-          <p className="project-description">
-            Web app that lets employees manage a hotel database system and
-            allows customers to search and book rooms based on several criteria.
-            Built with React.js for frontend, Node.js with Express for the
-            backend, and MySQL for DB, as a final project for the course CSI2132
-            - Databases I.
-          </p>
-          <ul>
-            <li>
-              <span className="list-text">React.js</span>
-            </li>
-            <li>
-              <span className="list-text">Node.js/Express</span>
-            </li>
-            <li>
-              <span className="list-text">MySQL</span>
-            </li>
-          </ul>
-        </div>
+        <Slideshow delay={4000}>
+          <ProjectItem
+            projectTitle="Globalization Index Analysis"
+            projectTechnologies="React.js, Node.js/Express, MySQL"
+            projectDescription={[
+              "Analyzed the effects of globalization on financial markets using KOF’s globalization index to measure levels of globalization and local stock market indices as proxies for their performance",
+              "Wrote a report on findings, including evaluation of several machine learning models trained with Scikit-Learn",
+              "Achieved an accuracy of 74% on a random forest binary classifier",
+            ]}
+            githubLink="https://github.com/andrewbapham/ehotels-app"
+          />
+          <ProjectItem
+            projectTitle="E-hotels App"
+            projectTechnologies="React.js, Node.js/Express, MySQL"
+            projectDescription={[
+              "Developed a full-stack web app that allows customers to search and book hotel rooms, and employees to create and manage hotel chains",
+              "Built a front-end using React, and a back-end in Node.js/Express using MySQL as a database",
+            ]}
+            githubLink="https://github.com/andrewbapham/ehotels-app"
+          />
+          <ProjectItem
+            projectTitle="Self-Hosted Websites"
+            projectTechnologies="React, HTML/CSS, Nginx, Linux"
+            projectDescription={[
+              "Built websites for family with React and self-hosted them on an Nginx server running on a Raspberry Pi",
+            ]}
+          />
+        </Slideshow>
       </div>
     </div>
   );
